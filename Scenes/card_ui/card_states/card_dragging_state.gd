0a50aa7e3363b890733bb4761d115694a43569a8
--help <gerwinmuller29@gmail.com>
2026-04-23 23:50:14 +0200

    card aiming

--- Scenes/card_ui/card_states/card_dragging_state.gd
+++ Scenes/card_ui/card_states/card_dragging_state.gd
@@ -18,18 +18,23 @@ func enter() -> void:
 	
 	
 func on_input(event: InputEvent) -> void:
-		var mouse_motion := event is InputEventMouseMotion
-		var cancel = event.is_action_pressed("right_mouse")
-		var confirm = event.is_action_released("left_mouse") or event.is_action_pressed("left_mouse")
+	var single_targeted := card_ui.card.is_single_targeted()
+	var mouse_motion := event is InputEventMouseMotion
+	var cancel = event.is_action_pressed("right_mouse")
+	var confirm = event.is_action_released("left_mouse") or event.is_action_pressed("left_mouse")
 		
-		if mouse_motion:
-			card_ui.global_position = card_ui.get_global_mouse_position() - card_ui.pivot_offset
-			
-		if cancel:
-			transition_requested.emit(self, CardState.State.BASE)
-		elif minimum_drag_time_elapsed and confirm:
-			get_viewport().set_input_as_handled()
-			transition_requested.emit(self, CardState.State.RELEASED)
+	if single_targeted and mouse_motion and card_ui.targets.size() > 0:
+		transition_requested.emit(self, CardState.State.AIMING)
+		return
+		
+	if mouse_motion:
+		card_ui.global_position = card_ui.get_global_mouse_position() - card_ui.pivot_offset
+		
+	if cancel:
+		transition_requested.emit(self, CardState.State.BASE)
+	elif minimum_drag_time_elapsed and confirm:
+		get_viewport().set_input_as_handled()
+		transition_requested.emit(self, CardState.State.RELEASED)
 
 
 # Called when the node enters the scene tree for the first time.
