e544815430e9711abb76c253334aeaef8b034dbe
Jonas Wernhardt <jonas.wernhardt@gmx.at>
2026-04-28 00:30:14 +0200

    Effects, Logic and Mana Management added

--- Scenes/card_ui/card_ui.gd
+++ Scenes/card_ui/card_ui.gd
@@ -6,7 +6,9 @@ signal reparent_requested(which_card_ui: CardUI)
 const BASE_STYLEBOX := preload("uid://rr31frftay8")
 const DRAG_STYLEBOX := preload("uid://citqi3a5bcbdy")
 const HOVER_STYLEBOX := preload("uid://n6upd2f74mrs")
+
 @export var card: Card : set = _set_card
+@export var char_stats: CharacterStats : set = _set_char_stats
 
 @onready var panel: Panel = $Panel
 @onready var cost: Label = $Cost
@@ -14,13 +16,20 @@ const HOVER_STYLEBOX := preload("uid://n6upd2f74mrs")
 @onready var drop_point_detector: Area2D = $DropPointDetector
 @onready var card_state_machine: CardStateMachine = $CardStateMachine as CardStateMachine
 @onready var targets: Array[Node] = []
+@onready var original_index := self.get_index()
 
 var parent: Control
 var tween: Tween
+var playable := true : set = _set_playable
+var disabled := false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Events.card_aim_started.connect(_on_card_drag_or_aiming_started)
+	Events.card_drag_started.connect(_on_card_drag_or_aiming_started)
+	Events.card_aim_ended.connect(_on_card_drag_or_aiming_ended)
+	Events.card_drag_ended.connect(_on_card_drag_or_aiming_ended)
 	card_state_machine.init(self)
 
 
@@ -30,6 +39,13 @@ func _input(event: InputEvent) -> void:
 func animate_to_position(new_position: Vector2, duration: float) -> void:
 	tween = create_tween().set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
 	tween.tween_property(self, "global_position", new_position, duration)
+
+func play() -> void:
+	if not card:
+		return
+		
+	card.play(targets, char_stats)
+	queue_free()
 
 func _on_gui_input(event: InputEvent) -> void:
 	card_state_machine.on_gui_input(event)
@@ -54,6 +70,21 @@ func _set_card(value: Card) -> void:
 	cost.text = str(card.cost)
 	icon.texture = card.icon
 
+func _set_playable(value: bool) -> void:
+	playable = value
+	if not playable:
+		cost.add_theme_color_override("font_color", Color.RED)
+		icon.modulate = Color(1, 1, 1, 0.5)
+	else:
+		cost.remove_theme_color_override("font_color")
+		icon.modulate = Color(1, 1, 1, 1)
+		
+		
+func _set_char_stats(value: CharacterStats) -> void:
+	char_stats = value
+	char_stats.stats_changed.connect(_on_char_stats_changed)
+	
+
 func _on_drop_point_detector_area_entered(area: Area2D) -> void:
 	if not targets.has(area):
 		targets.append(area)
@@ -61,3 +92,19 @@ func _on_drop_point_detector_area_entered(area: Area2D) -> void:
 
 func _on_drop_point_detector_area_exited(area: Area2D) -> void:
 	targets.erase(area)
+	
+
+func _on_card_drag_or_aiming_started(used_card: CardUI) -> void:
+		if used_card == self:
+			return
+			
+		disabled = true
+		
+		
+func _on_card_drag_or_aiming_ended(_card: CardUI) -> void:
+	disabled = false
+	self.playable = char_stats.can_play_card(card)
+	
+	
+func _on_char_stats_changed() -> void:
+	self.playable = char_stats.can_play_card(card)
