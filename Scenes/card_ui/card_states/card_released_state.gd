5a81155584d6d54a9413c32cdf600330240da145
Jonas Wernhardt <jonas.wernhardt@gmx.at>
2026-04-27 11:45:21 +0200

    Card Visuals updated

--- Scenes/card_ui/card_states/card_released_state.gd
+++ Scenes/card_ui/card_states/card_released_state.gd
@@ -3,8 +3,7 @@ extends CardState
 var played: bool
 
 func enter() -> void:
-	card_ui.color.color = Color.DARK_VIOLET
-	card_ui.state.text = "RELEASED"
+	
 	
 	played = false
 	
--- Scenes/card_ui/card_ui.gd
+++ Scenes/card_ui/card_ui.gd
@@ -3,10 +3,14 @@ extends Control
 
 signal reparent_requested(which_card_ui: CardUI)
 
-@export var card: Card
+const BASE_STYLEBOX := preload("uid://rr31frftay8")
+const DRAG_STYLEBOX := preload("uid://dntnlpqmyvv5a")
+const HOVER_STYLEBOX := preload("uid://b6hkx2byf1oit")
+@export var card: Card : set = _set_card
 
-@onready var color: ColorRect = $Color
-@onready var state: Label = $State
+@onready var panel: Panel = $Panel
+@onready var cost: Label = $Cost
+@onready var icon: TextureRect = $Icon
 @onready var drop_point_detector: Area2D = $DropPointDetector
 @onready var card_state_machine: CardStateMachine = $CardStateMachine as CardStateMachine
 @onready var targets: Array[Node] = []
@@ -42,6 +46,13 @@ func _on_mouse_exited() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _set_card(value: Card) -> void:
+	if not is_node_ready():
+		await ready
+		
+	card = value
+	cost.text = str(card.cost)
+	icon.texture = card.icon
 
 func _on_drop_point_detector_area_entered(area: Area2D) -> void:
 	if not targets.has(area):
