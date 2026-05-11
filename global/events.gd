abf5d8a714f0170313260489f7b2c55510ea55d8
annu <flaschenedition@gmail.com>
2026-05-11 02:24:35 +0200

    enemy ai and win/lose states

--- global/events.gd
+++ global/events.gd
@@ -11,3 +11,7 @@ signal tooltip_hide_requested
 signal player_hand_drawn
 signal player_hand_discarded
 signal player_turn_ended
+signal player_died
+
+signal enemy_action_completed(enemy: Enemy)
+signal enemy_turn_ended
