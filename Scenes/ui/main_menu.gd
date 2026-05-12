193242524484693db8ef5964a9e7830d69abcb3c
Jonas Wernhardt <jonas.wernhardt@gmx.at>
2026-05-12 20:29:28 +0200

    Main menu scene created

---
+++ Scenes/ui/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var continue_button: Button = %Continue
+
+func _ready() -> void:
+	get_tree().paused = false
+
+func _on_continue_pressed() -> void:
+	print("continue run")
+
+
+func _on_new_run_pressed() -> void:
+	print ("new run")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
