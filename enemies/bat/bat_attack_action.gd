5b3c89ab44b7364a649db1521384383fcbcade71
annu <flaschenedition@gmail.com>
2026-05-31 23:59:01 +0200

    sfx, music, new enemies, new attacks

--- enemies/bat/bat_attack_action.gd
+++ enemies/bat/bat_attack_action.gd
@@ -1,6 +1,6 @@
 extends EnemyAction
 
-@export var damage := 7
+@export var damage := 4
 
 
 func perform_action() -> void:
@@ -9,13 +9,15 @@ func perform_action() -> void:
 	
 	var tween := create_tween().set_trans(Tween.TRANS_QUINT)
 	var start := enemy.global_position
-	var end := target.global_position - Vector2.RIGHT * 32
+	var end := target.global_position + Vector2.RIGHT * 32
 	var damage_effect := DamageEffect.new()
 	var target_array: Array[Node] = [target]
 	damage_effect.amount = damage
 	damage_effect.sound = sound
 	
 	tween.tween_property(enemy, "global_position", end, 0.4)
+	tween.tween_callback(damage_effect.execute.bind(target_array))
+	tween.tween_interval(0.35)
 	tween.tween_callback(damage_effect.execute.bind(target_array))
 	tween.tween_interval(0.25)
 	tween.tween_property(enemy, "global_position", start, 0.4)
--- enemies/bat/bat_block_action.gd
+++ enemies/bat/bat_block_action.gd
@@ -1,19 +1,6 @@
 extends EnemyAction
 
-@export var block := 15
-@export var hp_threshold := 6
-
-var already_used := false
-
-
-func is_performable() -> bool:
-	if not enemy or already_used:
-		return false
-	
-	var is_low := enemy.stats.health <= hp_threshold
-	already_used = is_low
-	
-	return is_low
+@export var block := 4
 
 
 func perform_action() -> void:
